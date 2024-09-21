3fde86b1b9be6de4036f77e682d312d4d8ad4aa5
Deepakyadav34 <deepak747392@gmail.com>
2024-09-21 16:11:03 +0530

    main

--- src/main.jsx
+++ src/main.jsx
@@ -9,20 +9,50 @@ import UseRef from './components/UseRef';
 import UseState from './components/UseState';
 import './index.css';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <>
-      <Route path='/' element={<App />} />
-      <Route path='useState' element={<UseState />} />
-      <Route path='useEffect' element={<UseEffect />} />
-      <Route path='useRef' element={<UseRef />} />
-      <Route path='useCallback' element={<UseCallback />} />
-      <Route path='customHook' element={<CustomHook />} />
-      {/* <Route path='user/:userid' element={<User/>}/> */}
-      
-    </>
-  )
-);
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App />, // This is the main layout component
+//   },
+//   {
+//     path: 'useState', // This is the new page route
+//     element: <UseState />, // This renders the UseState component
+//   },
+//   {
+//     path:'useEffect',
+//     element:<UseEffect/>
+
+//   },
+//   {
+//     path:'useRef',
+//     element:<UseRef/>
+
+//   },
+//   {
+//     path:'useCallback',
+//     element:<UseCallback/>
+
+//   }
+//   ,{
+//     path:'customHook',
+//     element:<CustomHook/>
+//   }
+// ]);
+
+const router=createBrowserRouter(
+ createRoutesFromElements(
+  <>
+  <Route path='/' element={<App/>}/>,
+  <Route path='useState' element={<UseState/>}/>,
+  <Route path='useEffect' element={<UseEffect/>}/>,
+  <Route path='useCallback' element={<UseCallback/>}/>,
+  <Route path='useRef' element={<UseRef/>}/>,
+  <Route path='customHook' element={<CustomHook/>}/>
+
+  </>
+
+ )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
